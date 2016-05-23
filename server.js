15a105085e4fce844875e996b1432533b99d6f4a
Shawn Watson <shawnw86@gmail.com>
2016-05-23 11:30:12 -0700

    Hook up react component to emitter

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 var express = require('express')
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var r = require('rethinkdb')
+var connection = null
+var server = app.listen(3000, function(){
+	console.log('listening on :3000')
+})
+var io = require('socket.io').listen(server);
+
 
 app.use(express.static('src/client'));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname, '/src/client/index.html')
+r.connect({host: 'localhost'}, function(err, conn){
+	if (err) throw err;
+	connection = conn
+	r.table('likes').changes().run(conn, function(err, cursor){
+		if (cursor) {
+			console.log("Database Exists")
+			cursor.each(function(err, row){
+				if (err) throw err;
+				console.log(row.new_val.likeCount)
+				io.emit('incrementLikes', row.new_val.likeCount);
+			})
+		} else {
+			r.db('test').tableCreate('likes').run(connection, function(err, result){
+				if (err) throw err;
+				r.table('likes').insert({likeCount: 0}).run(connection, function (err, result){
+					if (err) throw err;
+					console.log('Successfully Created')
+				})
+			})
+		}
+	})
 })
 
-app.listen(3000, function(){
-	console.log('listening on :3000')
+io.on('connection', function(socket){
+	console.log('a user connected')
+	socket.on('like', function(){
+		r.table('likes').get(1).update({
+			likeCount: r.row("likeCount").add(1)
+		}).run(connection)
+	})
 })
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname, 'index.html')
+})
+
+
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx'
+import io from 'socket.io-client'
+const socket = io.connect(window.location.host)
 
-class App extends React.Component {
-  render () {
+var App = React.createClass({
+	getInitialState: function() {
+		return {likesCount: 0};
+	},
+	onLike: function() {
+		socket.emit('like');
+	},
+	componentDidMount: function() {
+		socket.on('incrementLikes', this._incrementLikes);
+	},
+	_incrementLikes: function(newLikeCount){
+		this.setState({likesCount: newLikeCount});
+	},
+  render: function() {
     return (
     <div>
     	<p>React Component 1</p>
-    	<AwesomeComponent />
+    	<p>Likes : {this.state.likesCount}</p>
+    	<div>
+    		<button onClick={this.onLike}>Like Me</button>
+    	</div>
     </div>
     )
   }
-}
+})
 
 render(<App/>, document.getElementById('app'));
