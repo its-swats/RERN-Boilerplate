f4a13a75699e38a37635163c9f886ca21eec8f91
Shawn Watson <shawnw86@gmail.com>
2016-05-23 21:16:47 -0700

    Update server to create default entry if it does not exist

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(express.static('src/client'));
 r.connect({host: 'localhost'}, function(err, conn){
 	if (err) throw err;
 	connection = conn
+	r.table('likes').get(1).run(conn, function(err, cursor){
+		if (cursor == null) {
+			r.table('likes').insert({id: 1, likeCount: 0}).run(conn)
+		}
+	})
 	r.table('likes').changes().run(conn, function(err, cursor){
 		cursor.each(function(err, row){
 			if (err) throw err;
