e77199682013678419d0d196461cac707dff30ba
Shawn Watson <shawnw86@gmail.com>
2016-05-25 13:59:59 -0700

    Extract socket emitters to src/server/socket.js

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 var express = require('express');
 var app = express();
 var r = require('rethinkdbdash')();
-var connection = null;
 var server = app.listen(3000, function(){
 	console.log('listening on :3000');
 })
 var io = require('socket.io').listen(server);
+var attachSocket = require('./src/server/socket.js')
 var databaseSetup = require('./src/server/database.js');
 
 app.use(express.static('src/client'));
-
 databaseSetup.prepareForLaunch(function(row){
 	io.emit('setLikes', row.new_val.likeCount);
 });
-
-io.on('connection', function(socket){
-	socket.on('like', function(){
-		r.table('likes').get(1).update({
-			likeCount: r.row("likeCount").add(1)
-		}).run(connection);
-	});
-});
+attachSocket(io);
 
 app.use('/', require('./src/server/routes'))
 
