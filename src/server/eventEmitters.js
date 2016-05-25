719bb691109f0911831bf5cd38178c2f6796e19d
Shawn Watson <shawnw86@gmail.com>
2016-05-25 14:43:43 -0700

    Add active connection tracking to the event handler

--- src/server/eventEmitters.js
+++ src/server/eventEmitters.js
@@ -1,12 +1,16 @@
 var r = require('rethinkdbdash')();
 
-var attachSocket = function(io){
+var attachSocket = function(io, activeConnections){
     io.on('connection', function(socket){
+        activeConnections += 1
         socket.on('like', function(){
             r.table('likes').get(1).update({
                 likeCount: r.row("likeCount").add(1)
             }).run();
         });
+        socket.on('disconnect', function(){
+            activeConnections -= 1
+        })
     });
 };
 
